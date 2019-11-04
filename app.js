b5741c5d4f5aa19c9687a3a271666a1e0dec2b7e
Ryo <zack7498@gmail.com>
2019-11-04 21:56:45 +0800

    [clean]

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 'use strcit';
 
 const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
+const middleware = require('./mod/middleware/index.js');
+const router = require('./route/index.js');
 const app = express();
+middleware(app);
+router(app);
 
-app.use(morgan('common'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-
-app.use('/account', require('./route/account.js'));
 app.use((req, res, next) => {
   return res.sendStatus(404);
 });
