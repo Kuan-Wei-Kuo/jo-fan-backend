0bfc011ff284bedfb5d76cb999618ab7d0adbd31
Ryo <zack7498@gmail.com>
2019-12-02 19:59:51 +0800

    [add][middleware] first version

--- mod/middleware/index.js
+++ mod/middleware/index.js
@@ -1,14 +1,13 @@
-'use strict';
-
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const filter = require('./filter/index.js');
+'use strict'
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const filter = require('./filter/index.js')
 
 module.exports = (app) => {
-  app.use(morgan('common'));
-  app.use(bodyParser.json());
+  app.use(morgan('common'))
+  app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({
     extended: false
-  }));
+  }))
   // app.use(filter());
-};
+}
